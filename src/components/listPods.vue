8b65bc602323c8831c03c93a281878374bd0515a
0xGUI <silva.guilherme@protonmail.ch>
2020-05-18 22:43:36 +0100

    Fix pod list

--- src/components/listPods.vue
+++ src/components/listPods.vue
@@ -2,24 +2,32 @@
   <div>
     <table class="table table-striped">
       <tr>
-        <th>Pods: </th>
+        <th>Pods:</th>
       </tr>
     </table>
     <table class="table table-striped">
       <thead>
         <tr>
+          <th>Namespace</th>
           <th>Name</th>
-          <th>NameSpace</th>
           <th>Containers</th>
           <th>Status</th>
+          <th>Restarts</th>
+          <th>Age</th>
+          <th>IP</th>
+          <th>Node</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="pod in pods" :key="pod.metadata.name">
+          <td>{{ pod.metadata.namespace }}</td>
           <td>{{ pod.metadata.name }}</td>
-          <td>{{ pod.metadata.uid }}</td>
           <td>{{ pod.spec.containers.length }}</td>
           <td>{{ pod.status.phase }}</td>
+          <td>{{ pod.status.containerStatuses[0].restartCount }}</td>
+          <td>{{ pod.metadata.creationTimestamp }}</td>
+          <td>{{ pod.status.podIP}}</td>
+          <td>{{ pod.spec.nodeName}}</td>
         </tr>
       </tbody>
     </table>
@@ -31,7 +39,6 @@ export default {
   data: function() {
     return {
       pods: null
-
     };
   },
   methods: {
@@ -39,7 +46,7 @@ export default {
       var axiosPods = this.axios.create({
         headers: {
           "Content-Type": "application/json",
-          "Accept": "*/*",
+          Accept: "*/*"
           //"x-auth-token": 'Bearer ' + this.$store.state.token
         }
       });
@@ -48,7 +55,7 @@ export default {
         .get("/api/v1/pods")
         .then(response => {
           this.pods = response.data.items;
-          console.log(this.pods)
+          console.log(this.pods);
         })
         .catch(error => {
           console.log("Failed to load Pods:");
