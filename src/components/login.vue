d9b4e430ae4d8af9a71b28de9bbd4935a29b4443
0xGUI <silva.guilherme@protonmail.ch>
2020-05-23 13:06:05 +0100

    Login almost done

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,28 +1,47 @@
 <template>
-  <div>
-    <h1 align="center">LOGIN</h1>
-    <div class="inputField">
-      <label for="token">Token:</label>
-      <input
-        type="text"
-        :token="token"
-        v-model="token"
-        @input="$emit('input', $event.target.value)"
-        @keyup.enter="userLogin()"
-      />
-      <button type="submit" class="btn btn-primary" @click="loginToken()">Login</button>
-    </div>
-    <br />
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Token"
+                    :token="token"
+                    v-model="token"
+                    @input="$emit('input', $event.target.value)"
+                    @keyup.enter="loginToken()"
+                    name="Token"
+                    type="password"
+                    prepend-icon="mdi-lock"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="loginToken()">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 
-    <div class="inputField">
+  <!-- <div class="inputField">
       <label class="text-reader">
         Or File:
-        <input type="file" @change="loadTextFromFile">
+        <input type="file" @change="loadTextFromFile" />
       </label>
       <button type="submit" class="btn btn-primary" @click="loginFile()">Login</button>
       <br />
-    </div>
-  </div>
+  </div>-->
 </template>
 <script>
 export default {
@@ -30,7 +49,7 @@ export default {
   data: function() {
     return {
       token: "",
-      file: null,
+      file: null
     };
   },
   methods: {
@@ -41,43 +60,43 @@ export default {
       this.file.onload = e => this.$emit("load", e.target.result);
       this.file.readAsText(txtFile);
     },
-    loginToken(){
+    loginToken() {
       var axiosLogin = this.axios.create({
         headers: {
-          'Authorization': 'Bearer ' + this.token
+          Authorization: "Bearer " + this.token
         }
       });
 
-      axiosLogin.get("/api")
-      .then(response => {
-        console.log(response)
-        this.$store.commit("setToken", this.token);
-      })
-      .catch(error => {
-        console.log("Error Login with Text")
-        console.log(error)
-      })
+      axiosLogin
+        .get("/api")
+        .then(response => {
+          console.log(response);
+          this.$store.commit("setToken", this.token);
+        })
+        .catch(error => {
+          console.log("Error Login with Text");
+          console.log(error);
+        });
     },
-    loginFile(){
+    loginFile() {
       var axiosLogin = this.axios.create({
         headers: {
-          'Authorization': 'Bearer ' + this.file.result,
+          Authorization: "Bearer " + this.file.result
         }
       });
 
-      axiosLogin.get("/api")
-      .then(response => {
-        console.log(response)
-        this.$store.commit("setToken", this.file.result);
-      })
-      .catch(error => {
-        console.log("Error Login with File")
-        console.log(error)
-      })
+      axiosLogin
+        .get("/api")
+        .then(response => {
+          console.log(response);
+          this.$store.commit("setToken", this.file.result);
+        })
+        .catch(error => {
+          console.log("Error Login with File");
+          console.log(error);
+        });
     }
   },
-  created(){
-
-  }
+  created() {}
 };
 </script>
